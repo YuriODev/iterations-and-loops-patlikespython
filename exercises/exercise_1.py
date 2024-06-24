88d2000875f3299b82981fe577b48bac7465b7fb
patlikespython <patryktolkin9@gmail.com>
2024-06-24 10:53:58 +0000

    new

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,6 +1,6 @@
 # Your solution to Exercise 1
-n=int(input("Enter seconds:     "))
-for i in range(n,0,-1):
+num=int(input("Enter seconds:     "))
+for i in range(num,0,-1):
   print(i)
 print("Start!")
 
--- exercises/exercise_18.py
+++ exercises/exercise_18.py
@@ -1,1 +1,7 @@
 # Your solution to Exercise 18
+days=int(input("Enter a number of days:    "))
+total_errors=0
+for _ in range(days):
+    errors=int(input("Enter number of errors:    "))
+    total_errors+=errors
+print(total_errors)
